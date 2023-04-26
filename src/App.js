654db4802bbfb0b00d12a5d0e18448d6e0c6ef79
shutter <6331335921@student.chula.ac.th>
2023-04-26 23:27:45 +0700

    improve seearch interface

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import myImage from "../images/default_img.jpg";
 
 function Home() {
   const [campgrounds, setCampgrounds] = useState([]);
@@ -22,14 +23,18 @@ function Home() {
     setSelectedProvince(event.target.value);
   }
 
+  let endpoint = "http://localhost:3000/api/v1/campgrounds";
+  if (selectedProvince !== "") {
+    endpoint += `?province=${selectedProvince}`;
+  }
+
   function handleSearch() {
     // Fetch campgrounds by selected province
-    fetch(
-      `http://localhost:3000/api/v1/campgrounds?province=${selectedProvince}`
-    )
+    fetch(endpoint)
       .then((response) => response.json())
       .then((data) => {
         setCampgrounds(data.data);
+        console.log(data.data);
       })
       .catch((error) => console.log(error));
   }
@@ -39,7 +44,7 @@ function Home() {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-6">
-            <div className="card p-3">
+            <div className="card p-3 mb-3">
               <div className="form-group">
                 <label htmlFor="province-select" className="search_label">
                   Please select the province you want to reserve.
@@ -50,7 +55,7 @@ function Home() {
                   value={selectedProvince}
                   onChange={handleProvinceChange}
                 >
-                  <option>-- Select Province --</option>
+                  <option value="">-- Select Province --</option>
                   {provinceOptions.map((province, index) => (
                     <option key={index} value={province}>
                       {province}
@@ -59,11 +64,7 @@ function Home() {
                 </select>
               </div>
 
-              <button
-                className="search_btn"
-                onClick={handleSearch}
-                disabled={!selectedProvince}
-              >
+              <button className="search_btn" onClick={handleSearch}>
                 Search
               </button>
             </div>
@@ -71,41 +72,95 @@ function Home() {
         </div>
       </div>
 
-      {campgrounds.length > 0 ? (
-        <div>
-          <h2>Our Camps</h2>
-          {campgrounds.map((campground) => (
-            <div key={campground._id} className="result_card">
-              <p>
-                <b>Name: </b>
-                {campground.name}
-              </p>
-              <p>
-                <b>Address: </b>
-                {campground.address}
-              </p>
-              <p>
-                <b>District: </b>
-                {campground.district}
-              </p>
-              <p>
-                <b>Region: </b>
-                {campground.region}
-              </p>
-              <p>
-                <b>Province: </b>
-                {campground.province}
-              </p>
-              <p>
-                <b>Postal Code: </b>
-                {campground.postalcode}
-              </p>
-            </div>
-          ))}
-        </div>
+      {selectedProvince === "" ? (
+        <>
+          <h2>All Camps</h2>
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {campgrounds.map((campground, index) => (
+              <div key={campground._id} className="col">
+                <div className="card mb-3 mt-0">
+                  {/* eslint-disable-next-line */}
+                  <img
+                    className="card-img-top"
+                    src={myImage}
+                    alt="Empty Image"
+                  />
+                  <div className="card-body">
+                    <p>
+                      <b>Name: </b>
+                      {campground.name}
+                    </p>
+                    <p>
+                      <b>Address: </b>
+                      {campground.address}
+                    </p>
+                    <p>
+                      <b>District: </b>
+                      {campground.district}
+                    </p>
+                    <p>
+                      <b>Region: </b>
+                      {campground.region}
+                    </p>
+                    <p>
+                      <b>Province: </b>
+                      {campground.province}
+                    </p>
+                    <p>
+                      <b>Postal Code: </b>
+                      {campground.postalcode}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
       ) : (
         <>
-          <h2>Campgrounds not available</h2>
+          <>
+            <h2>{campgrounds[0].province} Camps</h2>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+              {campgrounds.map((campground, index) => (
+                <div key={campground._id} className="col">
+                  <div className="card mb-3 mt-0">
+                    {/* eslint-disable-next-line */}
+                    <img
+                      className="card-img-top"
+                      src={myImage}
+                      alt="Empty Image"
+                    />
+                    <div className="card-body">
+                      <p>
+                        <b>Name: </b>
+                        {campground.name}
+                      </p>
+                      <p>
+                        <b>Address: </b>
+                        {campground.address}
+                      </p>
+                      <p>
+                        <b>District: </b>
+                        {campground.district}
+                      </p>
+                      <p>
+                        <b>Region: </b>
+                        {campground.region}
+                      </p>
+                      <p>
+                        <b>Province: </b>
+                        {campground.province}
+                      </p>
+                      <p>
+                        <b>Postal Code: </b>
+                        {campground.postalcode}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
         </>
       )}
     </div>
